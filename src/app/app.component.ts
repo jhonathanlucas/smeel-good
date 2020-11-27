6380a5896010da2d440a2d34ea521e4c10470b56
Jhonathan Lucas <jhow.basquete13@gmail.com>
2020-11-26 23:17:54 -0300

    chore: alterar configuração de cadastro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
@@ -11,7 +10,6 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 })
 
 export class AppComponent {
-  // rootPage:any = 'LoginPage';
   
   constructor(
     private platform: Platform,
--- src/app/cadastro/cadastro.page.ts
+++ src/app/cadastro/cadastro.page.ts
@@ -8,23 +8,66 @@ import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 })
 
 export class CadastroPage implements OnInit {
+  public formulariozinho = {
+    username: '',
+    email: '',
+    myDate: '',
+    cep: '',
+    number: '',
+    complement: '',
+    phone: '',
+    senha: '',
+    }
+
   ionicForm: FormGroup;
   defaultDate = "1987-06-30";
   isSubmitted = false;
+  // register: boolean;
+  // login: boolean;
+  storage: any;
 
   constructor(public formBuilder: FormBuilder) { }
 
+  // exibirResgistrar(){
+  //   this.login = false;
+  //   this.register = true;
+  // }
+  // exibirLogin(){
+  //   this.login = true;
+  //   this.register = false;
+  // }
   ngOnInit() {
-    this.ionicForm = this.formBuilder.group({
-      name: ['', [Validators.required, Validators.minLength(2)]],
-      email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
-      dob: [this.defaultDate],
-      mobile: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
-      cep: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
-      num: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
-      complemento: ['', [Validators.required, Validators.minLength(2)]]
-    })
+    // this.ionicForm = this.formBuilder.group({
+    //   name: ['', [Validators.required, Validators.minLength(2)]],
+    //   email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+    //   dob: [this.defaultDate],
+    //   mobile: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+    //   cep: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+    //   num: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+    //   complemento: ['', [Validators.required, Validators.minLength(2)]]
+    // })
   }
+
+  saveData(){
+    this.storage.set('key', this.formulariozinho);
+   
+  }
+
+  loadData()
+{
+  this.storage.get(this.key).then((val) => {
+    console.log('key', val);
+   
+  });
+}
+  key(key: any) {
+    throw new Error('Method not implemented.');
+  }
+  public logForm(){
+    console.log("informaçoes do cadastro: \n nome: " + this.formulariozinho)
+  
+  }
+  
 
   getDate(e) {
     let date = new Date(e.target.value).toISOString().substring(0, 10);
