3c0cd476dead8c295882fe802c1dd6cb30bce0d0
Lucaomena <lucaomena32@gmail.com>
2020-12-01 16:12:18 -0300

    feat: adicionando tela de carregamento.

---
+++ src/app/splashscreen/splashscreen-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { SplashscreenPage } from './splashscreen.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: SplashscreenPage
+  }
+];
+
+@NgModule({
+  
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
+  })
+export class SplashscreenPageRoutingModule {}
+  
+  
