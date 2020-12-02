da81cc6cdbaba49e5ab96e2ab80b819922dabc45
Gabriel0725 <gabriel.gmcontato@gmail.com>
2020-12-02 00:12:27 -0300

    descartando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,18 +16,6 @@ const routes: Routes = [
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
-    path: 'step1',
-    loadChildren: () => import('./step1/step1.module').then( m => m.Step1PageModule)
-  },
-  {
-    path: 'step2',
-    loadChildren: () => import('./step2/step2.module').then( m => m.Step2PageModule)
-  },
-  {
-    path: 'step3',
-    loadChildren: () => import('./step3/step3.module').then( m => m.Step3PageModule)
-  },
-  {
     path: 'cadastro',
     loadChildren: () => import('./cadastro/cadastro.module').then( m => m.CadastroPageModule)
   },
@@ -62,14 +50,12 @@ const routes: Routes = [
   {
     path: 'sobrenos',
     loadChildren: () => import('./sobrenos/sobrenos.module').then( m => m.SobrenosPageModule)
-  },  {
-    path: 'faleconosco',
-    loadChildren: () => import('./faleconosco/faleconosco.module').then( m => m.FaleconoscoPageModule)
   },
   {
     path: 'faleconosco',
     loadChildren: () => import('./faleconosco/faleconosco.module').then( m => m.FaleconoscoPageModule)
   },
+
 
 
  
